e5299cc75d7ccdeb967ae5873c1b8c867efb9ffa
JayashreeJanani <jayashreejanani.p@gmail.com>
2026-02-12 22:07:12 -0400

    Problem Statement prototype created

--- Assignment1.c
+++ Assignment1.c
@@ -3,13 +3,24 @@
 // Matrix Multiplication
 // Given two square matrices A and B (size N×N, type double), 
 // compute:C = A x B
-#define N =10;
+#define N 10
 //Building logic
+
 int main(){
 int i,j;
-int a[][]
-int b[][];
-int c[][];
+int a[N][N];
+int b[N][N];
+int c[N][N];
+for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+        a[i][j] = 1;
+    }
+}
+for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+        b[i][j] = 1;
+    }
+}
 for(i=0; i<N;i++){
     for(j=0; j<N; j++)
     c[i][j] = a[i][j] * b[i][j];
