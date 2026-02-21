92cb9373c91378c64ba17691ea424d8fe7470abb
JayashreeJanani <jayashree.199731@gmail.com>
2026-02-21 12:07:25 -0400

    Step 2: Initialization of matrices

--- a1.c
+++ a1.c
@@ -8,11 +8,34 @@
  2. Allocate A,B,C (dynamic 1D arrays)
  3. Initialize A and B 
  4. Run the selected mode
+ =====================================================================
+ Steps to be completed
+ Step 0: Understanding inputs/ outputs===============================100%
+ Step 1: Parse N, mode and allocate A,B, C memory space==============100%
+ Step 2: Initialization of matrices==================================100%
+ Step 3: Mode 0(Serial Baseline)
+ Step 4: Mode 1(threads + scheduling)
+ Step 5: Mode 2(Collapse(2))
+ Step 6: Mode 3(sync comparison: atomic vs critical)
+ Step 7: Mode 4(tasks)
+ Step 8: SIMD modes 
+ Step 9: Performance experiments
+
  */
 //TODO1: Step 0: Clean file structure
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+//4.1. Step 1C--> allocation of memory spaces of matrix A, B, C
+static double* alloc_matrix(int N) {
+    double *m = (double*)malloc((size_t)N * (size_t)N * sizeof(double));
+    //condition to check if malloc fails and returns NULL
+    if (m == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    return m;
+}
 int main(int argc, char **argv){
     printf("\n The number of arguments in argc = %d",argc);
     //1. the condition below checks whether this argc has < 3 values 
@@ -37,6 +60,10 @@ int main(int argc, char **argv){
         printf("Error! N should be greater than 0\n");
         return 1;
     }
+    //4. Step 1C: Allocate Matrices
+    double *A = alloc_matrix(N);
+    double *B = alloc_matrix(N);
+    double *C = alloc_matrix(N);
     
     return 0;
 }
